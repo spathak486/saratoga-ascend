b96c9c282fe2d4fa47d84e3946235b28f20eb01c
jaydeep kotak <jaydeepkotak1@gmail.com>
2026-08-18 00:32:23 +0530

    Updated changes

--- saratoga-ascend-be/config/plugins.ts
+++ saratoga-ascend-be/config/plugins.ts
@@ -39,6 +39,15 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
       },
     },
   },
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 10,
+      amountLimit: 100,
+    },
+  },
 });
 
 export default config;
