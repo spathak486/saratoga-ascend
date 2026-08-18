ec63435032548e6ade8597c2f4e42641b16f98a7
rani-abha <abharani0507@gmail.com>
2026-08-18 14:41:59 +0530

    added general link component

--- saratoga-ascend-be/types/generated/components.d.ts
+++ saratoga-ascend-be/types/generated/components.d.ts
@@ -1,3 +1,29 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedGeneralLink extends Struct.ComponentSchema {
+  collectionName: 'components_shared_general_links';
+  info: {
+    displayName: 'General Link';
+    icon: 'attachment';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    href: Schema.Attribute.String & Schema.Attribute.Required;
+    icon: Schema.Attribute.Media<'images'>;
+    isExternal: Schema.Attribute.Boolean &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String & Schema.Attribute.Required;
+    target: Schema.Attribute.Enumeration<['_self', '_blank']> &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<'_self'>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export namespace Public {
+    export interface ComponentSchemas {
+      'shared.general-link': SharedGeneralLink;
+    }
+  }
+}
